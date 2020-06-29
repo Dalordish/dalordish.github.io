e30520d1499cbc7595b9ba8602338cd854646c27
Dalordish <dalordish@gmail.com>
2020-06-29 13:14:49 +1000

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       twitter: `kylemathews`,
     },
   },
-  plugins: [
+  plugins: [`gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
